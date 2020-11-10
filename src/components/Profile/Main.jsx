14c4e6c194bc6928809dd32f56072917653e3b41
p1a2u3l4 <psilev97@mail.ru>
2020-11-10 09:10:57 +0200

    add router

--- src/components/Profile/Main.jsx
+++ src/components/Profile/Main.jsx
@@ -4,7 +4,7 @@ import Myposts from '../Posts/MyPosts'
 const Main = () => {
     return(
       <div className="main">
-        <img src="https://miro.medium.com/max/10368/1*an7X6yxD1YrMQVTaIg3uWw.jpeg"></img>
+        
         <div className="profilename">
           <div className="avatar"><img src="https://i.stack.imgur.com/uLdOr.png"></img></div>
           <div className="name_surname"><h2>Paul Silev</h2></div>

a509423c57ae7433e2bfc883296cfddc26977839
p1a2u3l4 <psilev97@mail.ru>
2020-11-10 08:55:54 +0200

    Add Routing and Dialogs component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Header from './components/Header/Header'
 import Nav from './components/Navbar/Nav'
 import Main from './components/Profile/Main'
 import Dialogs from './components/Dialogs/Dialogs'
+import {BrowserRouter, Route} from "react-router-dom"
 
 const App = () => {
   return (
-    <div className="App app-wrapper">
-      <Header />
-      <Nav />
-      {/*<Main/> */}
-      <Dialogs />
-    </div>
+    <BrowserRouter>
+      <div className="App app-wrapper">
+        <Header />
+        <Nav />
+        <Route path="/Messanger" component={Dialogs}/>
+        <Route path="/Profile" component={Main}/>
+        {/*<Main/> */}
+        {/*<Dialogs />*/}
+      </div>
+    </BrowserRouter> 
   );
 }
 
